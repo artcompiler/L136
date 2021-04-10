70da6bef6184c527ee0479a49e3c765cefccca27
Jeff Dyer <jeffdyer@acm.org>
2021-04-10 09:37:35 -0700

    Init L136

--- src/compile.js
+++ src/compile.js
@@ -3,6 +3,6 @@ import {assert, message, messages, reserveCodeRange} from "./share.js";
 import {Compiler as BasisCompiler} from '@graffiticode/basis/compiler.js';
 //import {Compiler as BasisCompiler} from '../../../../work/graffiticode/basis/src/compiler.js';
 export const compiler = new BasisCompiler({
-  langID: 0,
+  langID: 136,
   version: 'v0.0.0',
 });
--- src/viewer.jsx
+++ src/viewer.jsx
@@ -3,7 +3,29 @@ import * as d3 from 'd3';
 //import {Viewer as BasisViewer} from '@graffiticode/basis/src/viewer.jsx';
 import {Viewer as BasisViewer} from '../../../../work/graffiticode/basis/src/viewer.jsx';
 import './style.css';
-class Viewer extends BasisViewer {};
+class Viewer extends BasisViewer {
+  componentDidMount() {
+    d3.select('#graff-view').append('div').classed('done-rendering', true);
+  }
+  renderElements(data) {
+    const elts = [];
+    data.forEach(d => {
+      elts.push(d);
+    });
+    return elts;
+  }
+  render() {
+    const props = this.props;
+    const data = props.obj && [].concat(props.obj) || [];
+    const elts = this.renderElements(data);
+    return (
+      <div>{elts}</div>
+    );
+  }
+};
+
+
+
 window.gcexports.viewer = {
   Viewer: Viewer
 };

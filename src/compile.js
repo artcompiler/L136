19facc947ab4831cce17a70e5eb578c3e59518e3
Jeff Dyer <jeffdyer@acm.org>
2021-04-10 17:24:45 -0700

    fix build

--- src/compile.js
+++ src/compile.js
@@ -1,6 +1,6 @@
 /* Copyright (c) 2021, ARTCOMPILER INC */
 import {assert, message, messages, reserveCodeRange} from "./share.js";
-import {Compiler as BasisCompiler} from '@graffiticode/basis/compiler.js';
+import {Compiler as BasisCompiler} from '@graffiticode/basis';
 //import {Compiler as BasisCompiler} from '../../../../work/graffiticode/basis/src/compiler.js';
 export const compiler = new BasisCompiler({
   langID: 136,
